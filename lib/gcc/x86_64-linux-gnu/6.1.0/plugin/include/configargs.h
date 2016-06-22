01bba5f1b43095e630345e60dc40d3b696e5e311
sayeed99 <sayeed.afridi2009@gmail.com>
2016-06-22 14:41:29 +0530

    Upload my new gcc-6.1.0 toolchain

---
+++ lib/gcc/x86_64-linux-gnu/6.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-6.1.0/configure --target=x86_64-linux-gnu --prefix=/home/cyborg/zenfone/toolchian6/x86_64-gcc-6.1.0/x86_64-linux-gnu --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-newlib --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --enable-cloog-backend=isl --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --disable-libitm --with-arch=x86-64 --with-tune=intel --with-fpmath=sse --with-multilib-list=m32,m64,mx32 --enable-initfini-array --disable-nls --with-sysroot=/ --with-python --with-bugurl=sayeed.afridi2009@gmail.com --enable-languages=c,c++ --disable-bootstrap --enable-plugins --enable-libgomp --enable-gnu-indirect-function --disable-libcilkrts --disable-libsanitizer --enable-gold --enable-threads --enable-graphite=yes --enable-eh-frame-hdr-for-static --enable-gold=default";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "intel" } };
